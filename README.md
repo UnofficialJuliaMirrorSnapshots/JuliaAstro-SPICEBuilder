a316b485b9b6b39df28045fe47047eaed5e19485
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-01 01:26:01 +0000

    Snapshot of branch master taken on 2019-04-30T21:26:00.808-04:00 from "https://github.com/UnofficialJuliaMirror/JuliaAstro-SPICEBuilder"

---
+++ README.md
@@ -0,0 +1,5 @@
+# SPICEBuilder
+
+[![Build Status](https://travis-ci.org/JuliaAstro/SPICEBuilder.svg?branch=master)](https://travis-ci.org/JuliaAstro/SPICEBuilder)
+
+This repository builds binary artifacts for the [SPICE.jl](https://github.com/JuliaAstro/SPICE.jl) project.
---
+++ build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "cspice"
+version = v"66"
+
+# Collection of sources required to build cspice
+sources = [
+    "http://naif.jpl.nasa.gov/pub/naif/toolkit//C/PC_Linux_GCC_64bit/packages/cspice.tar.Z" =>
+    "93cd4fbce5818f8b7fecf3914c5756b8d41fd5bdaaeac1f4037b5a5410bc4768",
+    "./extra"
+]
+
+# These are the platforms built inside the wizard
+platforms = supported_platforms()
+
+script = raw"""
+cd $WORKSPACE/srcdir
+cp -r cspice/src/cspice/ .
+
+# Create generated cmake files outside of source tree
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+make -j${nproc} VERBOSE=1
+make install VERBOSE=1
+"""
+
+products = prefix -> [
+    LibraryProduct(prefix, "libcspice", :libcspice)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = []
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
